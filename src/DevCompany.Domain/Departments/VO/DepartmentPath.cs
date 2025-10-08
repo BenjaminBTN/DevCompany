faf0f0b8a5e215d704701693453be24e23cc5d31
BenjaminBTN <galkin_aleksey@inbox.ru>
2025-10-08 11:35:40 +0300

    DS-3 add entities to domain\locations | refactoring

--- src/DevCompany.Domain/Departments/VO/DepartmentPath.cs
+++ src/DevCompany.Domain/Departments/VO/DepartmentPath.cs
@@ -1,20 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text.RegularExpressions;
-using System.Threading.Tasks;
+﻿using System.Text.RegularExpressions;
 using CSharpFunctionalExtensions;
 
 namespace DevCompany.Domain.Departments.VO;
 
 public record DepartmentPath
 {
+    public string Value { get; }
+
     private DepartmentPath(string value)
     {
         Value = value;
     }
-
-    public string Value { get; }
 
     public static Result<DepartmentPath> Create(string value)
     {
---
+++ src/DevCompany.Domain/Shared/VO/Timezone.cs
@@ -0,0 +1,24 @@
+﻿using System.Text.RegularExpressions;
+using CSharpFunctionalExtensions;
+
+namespace DevCompany.Domain.Shared.VO;
+
+public record Timezone
+{
+    public const string IANA_PATTERN = "([a-zA-Z0-9_])(\\/[a-zA-Z0-9_]+)";
+
+    public string Value { get; }
+
+    private Timezone(string value)
+    {
+        Value = value;
+    }
+
+    public static Result<Timezone> Create(string value)
+    {
+        if (Regex.Match(value, IANA_PATTERN).Success == false)
+            return Result.Failure<Timezone>("Invalid " + nameof(Timezone) + ".");
+
+        return new Timezone(value);
+    }
+}

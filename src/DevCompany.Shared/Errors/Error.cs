d525fc075bc96baf7b76f0bfbf247253a623556d
Galkin Aleksey <galkin_aleksey@inbox.ru>
2025-11-16 14:35:44 +0300

    DS-6 add Error.cs

--- src/DevCompany.Shared/Errors/Error.cs
+++ src/DevCompany.Shared/Errors/Error.cs
@@ -2,5 +2,36 @@
 
 public class Error
 {
-    
+    private Error(ErrorType type, string code, string message, string? invalidValue = null)
+    {
+        Type = type;
+        Code = code;
+        Message = message;
+        InvalidField = invalidValue;
+    }
+
+    public ErrorType Type { get; }
+    public string Code { get; }
+    public string Message { get; }
+    public string? InvalidField { get; }
+
+    public static Error Validation(string code, string message, string? invalidField = null) 
+        => new(ErrorType.VALIDATION, code, message, invalidField);
+
+    public static Error NotFound(string code, string message)
+        => new(ErrorType.NOT_FOUND, code, message);
+
+    public static Error Conflict(string code, string message)
+        => new(ErrorType.CONFLICT, code, message);
+
+    public static Error Failure(string code, string message)
+        => new(ErrorType.FAILURE, code, message);
+}
+
+public enum ErrorType
+{
+    VALIDATION,
+    NOT_FOUND,
+    CONFLICT,
+    FAILURE,
 }

d55d72a1f78e5a2132b01a9a7cb312934d741c77
BenjaminBTN <galkin_aleksey@inbox.ru>
2025-11-18 23:51:26 +0300

    DS-6 add SuccsessResult.cs | change .net version of Shared project

---
+++ src/DevCompany.Presentation/EndpointResults/SuccsessResult.cs
@@ -0,0 +1,21 @@
+﻿using DevCompany.Shared;
+
+namespace DevCompany.Presentation.EndpointResults;
+
+public sealed class SuccsessResult<TValue> : IResult
+{
+    private readonly TValue _value;
+
+    public SuccsessResult(TValue value) => _value = value;
+
+    public Task ExecuteAsync(HttpContext httpContext)
+    {
+        ArgumentNullException.ThrowIfNull(httpContext);
+
+        httpContext.Response.StatusCode = StatusCodes.Status200OK;
+
+        var envelope = Envelope.Ok(_value);
+
+        return httpContext.Response.WriteAsJsonAsync(envelope);
+    }
+}

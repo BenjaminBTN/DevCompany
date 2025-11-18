8c10ddefab8b379d927a0e227d6c7491ca09490e
BenjaminBTN <galkin_aleksey@inbox.ru>
2025-11-18 18:43:18 +0300

    DS-6 add Errors.cs

---
+++ src/DevCompany.Shared/Errors/Errors.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+
+namespace DevCompany.Shared.Errors;
+
+public class Errors : IEnumerable<Error>
+{
+    private readonly List<Error> _errors;
+
+    public Errors(IEnumerable<Error> errors)
+    {
+        _errors = errors.ToList();
+    }
+
+    public IEnumerator<Error> GetEnumerator()
+    {
+        return _errors.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+}
